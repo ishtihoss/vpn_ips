750bd6d387dbc741646b53f4aaf8aba03ffd6296
ishtihoss <45158028+ishtihoss@users.noreply.github.com>
2021-05-05 19:21:49 +0800

    made tokenizing more efficient

--- vpn_ips_pyspark_v1.3.py
+++ vpn_ips_pyspark_v1.3.py
@@ -19,6 +19,19 @@ df_app = df_app.select('app_name','bundle','description')
 df_app = df_app.withColumn("desc",F.lower(F.col("description")))
 df_app = df_app.withColumn("name",F.lower(F.col("app_name")))
 
+
+# Count VPN appearance in app description
+
+df_app = df_app.withColumn('vpn', lit('vpn')) # lit is the string you want to detect
+
+
+app_desc = df_app.filter(df_app['desc'].contains(df_app['vpn'])).groupBy('bundle').count()
+app_desc = app_desc.withColumnRenamed('count','desc_count')
+app_name = df_app.filter(df_app['name'].contains(df_app['vpn'])).groupBy('bundle').count()
+app_name = app_name.withColumnRenamed('count','name_count')
+join_count = app_desc.join(app_name, on='bundle', how='left')
+
+
 # df_app.printSchema()
 #root
 # |-- app_name: string (nullable = true)
@@ -48,12 +61,19 @@ joined_df = df_app.join(df, on='bundle', how='left')
 
 df_v1 = joined_df.select('ip','bundle','name','desc').where("bundle like '%vpn%' OR name like '%vpn%' OR desc like '%vpn%'")
 
+df_vX = df_v1.join(join_count, on='bundle', how='left')
+
 #Tokenize words in the description column to find how many times "vpn" occures
 # in the description
 
 
-df_app_desc_wc = df_app.withColumn('desc_word', F.explode(F.split(F.col('desc'), ' '))).filter("desc_word == 'vpn'").groupBy('desc_word','bundle').count().sort('count', ascending=False)
-df_app_name_wc = df_app.withColumn('name_word', F.explode(F.split(F.col('name'), ' '))).filter("name_word == 'vpn'").groupBy('name_word','bundle').count().sort('count', ascending=False)
+#df_app_desc_wc = df_app.withColumn('desc_word', F.explode(F.split(F.col('desc'), ' '))).filter("desc_word == 'vpn'").groupBy('desc_word','bundle').count().sort('count', ascending=False)
+#df_app_name_wc = df_app.withColumn('name_word', F.explode(F.split(F.col('name'), ' '))).filter("name_word == 'vpn'").groupBy('name_word','bundle').count().sort('count', ascending=False)
+
+
+
+
+
 
 
 ## Adding tokenized words to data DataFrame
@@ -63,7 +83,7 @@ df_v3 = df_v1.join(df_app_name_wc, on = 'bundle', how = 'left')
 
 ## Write file
 
-df_v3.write.format("parquet").option("compression", "snappy").save('s3a://ada-dev/ishti/vpn_household_31/')
+df_v3.write.format("parquet").option("compression", "snappy").save('s3a://ada-dev/ishti/vpn_household_X1/')
 
 #df_v2 = df_v1.select('bundle',F.explode('description')).alias('word'))
 #df_v3 = df_v2.select('bundle','word').filter("word == 'vpn'" OR "word == 'VPN'")\
